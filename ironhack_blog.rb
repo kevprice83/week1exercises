66c5a95444acbae345ed1303b50dd78836c2ce71
Kev Price <kev.price83@gmail.com>
2015-06-18 19:33:41 +0200

    blog exercise

--- ironhack_blog.rb
+++ ironhack_blog.rb
@@ -9,27 +9,32 @@ class Blog
 
 	def publish_front_page
 		@blog_posts.each do |post|
-		puts post.title
-		puts "***********"
-		puts "#{post.date}: #{post.text}"
-		puts "-----------"
+			if  post.sponsored
+				puts "******#{post.title}********"
+			else
+				puts post.title
+			end
+			puts "***********"
+			puts "#{post.date}: #{post.text}"
+			puts "-----------"
 		end
 	end
 end
 
 class Post
-	attr_accessor :title, :date, :text
-	def initialize(title, date, text)
+	attr_accessor :title, :date, :text, :sponsored
+	def initialize(title, date, text, sponsored)
 		@title = title
 		@date = date
 		@text = text
+		@sponsored = sponsored
 	end
 end
 
 ironhack_blog = Blog.new
-post1 = Post.new("I love Ruby", "16th June", "But it's so fucking confusing")
-post2 = Post.new("Ruby blew my mind!", "15th June", "Soooo many functions within one method...")
-post3 = Post.new("Progression", "14th June", "Baby steps.")
+post1 = Post.new("I love Ruby", "16th June", "But it's so fucking confusing", false)
+post2 = Post.new("Ruby blew my mind!", "15th June", "Soooo many functions within one method...", true)
+post3 = Post.new("Progression", "14th June", "Baby steps.", false)
 ironhack_blog.add_post(post1)
 ironhack_blog.add_post(post2)
 ironhack_blog.add_post(post3)
